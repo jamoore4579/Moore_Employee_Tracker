f74881c7d9ad27bec3b97625900dd3d2915f04c0
Irish Moore <jasonamoore@frontier.com>
2022-02-17 17:23:59 -0500

    sql tables are built

---
+++ db/schema.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS employee_role;
+DROP TABLE is EXISTS employee;
+
+CREATE TABLE department (
+    id INT PRIMARY KEY,
+    name VARCHAR(30) -- hold department name
+);
+	
+CREATE TABLE employee_role (
+    id INT PRIMARY KEY,
+    title VARCHAR(30), -- hold role title
+    salary DECIMAL, -- hold role salary
+    department_id INT -- hold reference to department role belongs to
+);
+	 
+CREATE TABLE employee (
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30), -- hold employee first name
+    last_name VARCHAR(30), -- hold employee last name
+    role_id INT, -- hold reference to employee role
+    manager_id INT -- hold reference to another employee that is the manager of the current employee 
+                   -- (null if the employee has no manager)
+);
