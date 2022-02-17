9f3d15330f6554a33b2eaa1ec90884ac1a473615
Irish Moore <jasonamoore@frontier.com>
2022-02-17 17:06:59 -0500

    setup project, server connection, and database

---
+++ db/connection.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2');
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'indiana1',
+      database: 'employee_tracker'
+    },
+    console.log('Connected to the Employee Tracker database.')
+  );
+
+module.exports = db;
