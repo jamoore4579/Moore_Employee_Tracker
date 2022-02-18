609fcfe49f48d688936e5f79ec34c909ba865b43
Irish Moore <jasonamoore@frontier.com>
2022-02-17 19:46:14 -0500

    first input question is added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const db = require('./db/connection');
 const table = require('console.table');
 const figlet = require('figlet');
+const inquirer = require('inquirer');
 
 // database connection
 db.connect((error) => {
@@ -19,3 +20,23 @@ db.connect((error) => {
   promptUser()
 });
 
+// prompt user for choice of action
+const promptUser = () => {
+  inquirer.prompt([
+    {
+      name: 'choices',
+      type: 'list',
+      message: 'Please select a choice from the list:',
+      choices: [
+        'View all Departments',
+        'View all Roles', 
+        'View all Employees', 
+        'Add Department', 
+        'Add Role', 
+        'Add Employee', 
+        'Update an Employee Role'
+      ]
+    }
+  ])
+  
+};
